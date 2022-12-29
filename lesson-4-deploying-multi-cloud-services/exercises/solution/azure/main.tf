3ef2ff2512fe593b77895e7dd67935aaa50c2584
Travis Scotto <travisscotto@macbook-pro-2.mynetworksettings.com>
2022-12-28 19:55:52 -0500

    Azure solution

--- lesson-4-deploying-multi-cloud-services/exercises/solution/azure/main.tf
+++ lesson-4-deploying-multi-cloud-services/exercises/solution/azure/main.tf
@@ -2,3 +2,50 @@ resource "azurerm_resource_group" "udacity" {
   name     = "udacity-resources"
   location = "North Central US"
 }
+
+data "azurerm_cosmosdb_account" "example" {
+  name                = "tfex-cosmosdb-account"
+  resource_group_name = "tfex-cosmosdb-account-rg"
+}
+
+resource "azurerm_cosmosdb_mongo_database" "example" {
+  name                = "tfex-cosmos-mongo-db"
+  resource_group_name = data.azurerm_cosmosdb_account.example.resource_group_name
+  account_name        = data.azurerm_cosmosdb_account.example.name
+}
+
+resource "azurerm_cosmosdb_mongo_collection" "example" {
+  name                = "tfex-cosmos-mongo-db"
+  resource_group_name = data.azurerm_cosmosdb_account.example.resource_group_name
+  account_name        = data.azurerm_cosmosdb_account.example.name
+  database_name       = azurerm_cosmosdb_mongo_database.example.name
+
+  default_ttl_seconds = "777"
+  shard_key           = "uniqueKey"
+  throughput          = 400
+
+  index {
+    keys   = ["_id"]
+    unique = true
+  }
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "example-app-service-plan"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Windows"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_windows_function_app" "example" {
+  name                = "example-windows-function-app"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
+
+  site_config {}
+}
