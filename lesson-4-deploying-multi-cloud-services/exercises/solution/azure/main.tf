f1182dce3831dcd2f02abeaa1802ddfd22e48901
Travis Scotto <travisscotto@macbook-pro-2.mynetworksettings.com>
2023-01-17 22:40:46 -0500

    Main changes

--- lesson-4-deploying-multi-cloud-services/exercises/solution/azure/main.tf
+++ lesson-4-deploying-multi-cloud-services/exercises/solution/azure/main.tf
@@ -1,81 +1,7 @@
 resource "azurerm_resource_group" "udacity" {
   name     = "udacity-resources"
-  location = "North Central US"
+  location = "US East 2"
 }
-
-# resource "random_integer" "ri" {
-#   min = 10000
-#   max = 99999
-# }
-
-# resource "azurerm_cosmosdb_account" "db" {
-#   name                = "tfex-cosmos-db-${random_integer.ri.result}"
-#   location            = azurerm_resource_group.udacity.location
-#   resource_group_name = azurerm_resource_group.udacity.name
-#   offer_type          = "Standard"
-#   kind                = "MongoDB"
-
-#   enable_automatic_failover = true
-
-#   capabilities {
-#     name = "EnableAggregationPipeline"
-#   }
-
-#   capabilities {
-#     name = "mongoEnableDocLevelTTL"
-#   }
-
-#   capabilities {
-#     name = "MongoDBv3.4"
-#   }
-
-#   capabilities {
-#     name = "EnableMongo"
-#   }
-
-#   consistency_policy {
-#     consistency_level       = "BoundedStaleness"
-#     max_interval_in_seconds = 300
-#     max_staleness_prefix    = 100000
-#   }
-
-#   geo_location {
-#     location          = "eastus"
-#     failover_priority = 1
-#   }
-
-#   geo_location {
-#     location          = "westus"
-#     failover_priority = 0
-#   }
-# }
-
-# data "azurerm_cosmosdb_account" "example" {
-#   name                = azurerm_cosmosdb_account.db.name
-#   resource_group_name = azurerm_resource_group.udacity.name
-# }
-
-# resource "azurerm_cosmosdb_mongo_database" "example" {
-#   name                = "tfex-cosmos-mongo-db"
-#   resource_group_name = data.azurerm_cosmosdb_account.example.resource_group_name
-#   account_name        = data.azurerm_cosmosdb_account.example.name
-# }
-
-# resource "azurerm_cosmosdb_mongo_collection" "example" {
-#   name                = "tfex-cosmos-mongo-db"
-#   resource_group_name = data.azurerm_cosmosdb_account.example.resource_group_name
-#   account_name        = data.azurerm_cosmosdb_account.example.name
-#   database_name       = azurerm_cosmosdb_mongo_database.example.name
-
-#   default_ttl_seconds = "777"
-#   shard_key           = "uniqueKey"
-#   throughput          = 400
-
-#   index {
-#     keys   = ["_id"]
-#     unique = true
-#   }
-# }
 
 resource "azurerm_storage_account" "example" {
   name                     = "udacityfunctionapp"
@@ -102,6 +28,9 @@ resource "azurerm_windows_function_app" "example" {
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
   service_plan_id            = azurerm_service_plan.example.id
 
+  application "stack" {
+    powershell_core_version = 7
+  }
   site_config {}
 }
 
@@ -113,8 +42,8 @@ resource "azurerm_kubernetes_cluster" "example" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
+    node_count = 3
+    vm_size    = "Standard_D2s_v3"
   }
 
   identity {
