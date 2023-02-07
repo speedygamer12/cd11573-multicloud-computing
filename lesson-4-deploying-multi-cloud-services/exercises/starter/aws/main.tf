fbacadd81e0efe857f2fb9b023bf2358c477486f
Travis Scotto <travisscotto@macbook-pro-2.mynetworksettings.com>
2023-02-06 19:44:09 -0500

    Adding headings

--- lesson-4-deploying-multi-cloud-services/exercises/starter/aws/main.tf
+++ lesson-4-deploying-multi-cloud-services/exercises/starter/aws/main.tf
@@ -67,3 +67,5 @@ resource "aws_route_table_association" "private" {
   subnet_id      = element(aws_subnet.private.*.id, count.index)
   route_table_id = element(aws_route_table.private.*.id, count.index)
 }
+
+##### Your code starts here #####
