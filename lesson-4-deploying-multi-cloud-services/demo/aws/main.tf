03b35600055259c8f7e80f6ef675b377b1c96898
Travis Scotto <travisscotto@macbook-pro-2.mynetworksettings.com>
2023-02-07 16:17:04 -0500

    Adding EC2

--- lesson-4-deploying-multi-cloud-services/demo/aws/main.tf
+++ lesson-4-deploying-multi-cloud-services/demo/aws/main.tf
@@ -89,7 +89,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  subnet_id = aws_subnet.public[0]
+  subnet_id = aws_subnet.public[0].id
 
   tags = {
     Name = "HelloWorld"
