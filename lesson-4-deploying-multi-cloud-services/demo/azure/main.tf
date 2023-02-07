d712b8f7f7faac333b0ccc3b1191841c9b2ec1e9
Travis Scotto <travisscotto@macbook-pro-2.mynetworksettings.com>
2023-02-07 16:48:14 -0500

    Azure

--- lesson-4-deploying-multi-cloud-services/demo/azure/main.tf
+++ lesson-4-deploying-multi-cloud-services/demo/azure/main.tf
@@ -1,6 +1,35 @@
-resource "azurerm_resource_group" "udacity" {
-  name     = "udacity-resources"
-  location = "North Central US"
+data "azurerm_resource_group" "example" {
+  name     = "Regroup_4gKqrgD_cn"
+  location = "West US"
 }
 
 ##### Your code starts here #####
+
+
+resource "azurerm_storage_account" "example" {
+  name                     = "windowsfunctionappsa"
+  resource_group_name      = data.azurerm_resource_group.example.name
+  location                 = data.azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "example-app-service-plan"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  os_type             = "Windows"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_windows_function_app" "example" {
+  name                = "example-windows-function-app"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
+
+  site_config {}
+}
