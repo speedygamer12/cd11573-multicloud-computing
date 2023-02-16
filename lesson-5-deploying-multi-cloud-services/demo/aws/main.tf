af2673b3b8a85746b0a15592fd44067cc806ee80
Travis Scotto <travisscotto@macbook-pro-2.mynetworksettings.com>
2023-02-16 15:27:03 -0500

    Adding demo

--- lesson-5-deploying-multi-cloud-services/demo/aws/main.tf
+++ lesson-5-deploying-multi-cloud-services/demo/aws/main.tf
@@ -69,3 +69,29 @@ resource "aws_route_table_association" "private" {
 }
 
 ##### Your code starts here #####
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.public[0].id
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
